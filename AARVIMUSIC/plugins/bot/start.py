6d471b1960bbbebb2697171a14ae7b0f35cf186c
- ʙᴧᴃʏ ηєᴛᴡσᴙᴋ /³ <mahtoxanjali@gmail.com>
2025-03-06 07:09:26 +0530

    - ʙᴧᴃʏ ηєᴛᴡσᴙᴋ </³

--- AARVIMUSIC/plugins/bot/start.py
+++ AARVIMUSIC/plugins/bot/start.py
@@ -5,7 +5,6 @@ from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
 import config
 from AARVIMUSIC import app
 from AARVIMUSIC.misc import _boot_
@@ -26,19 +25,15 @@ from AARVIMUSIC.utils.inline import help_pannel, private_panel, start_panel
 from config import BANNED_USERS
 from strings import get_string
 
-#--------------------------
 
-NEXI_VID = [
-"https://files.catbox.moe/rp5ai8.mp4",
-"https://files.catbox.moe/yh19xh.mp4",
-"https://files.catbox.moe/6t7n6u.mp4",
-"https://files.catbox.moe/tc7tjs.mp4",
-"https://files.catbox.moe/4p5os4.mp4",
-"https://files.catbox.moe/58c1cj.mp4",
-"https://files.catbox.moe/wh9pgk.mp4",
-"https://files.catbox.moe/vh49fu.mp4",
+
+YUMI_PICS = [
+"https://files.catbox.moe/iqs0hw.jpg",
+"https://files.catbox.moe/tt257i.jpg",
 
 ]
+
+
 
 @app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
@@ -48,8 +43,8 @@ async def start_pm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            return await message.reply_video(
-                random.choice(NEXI_VID),
+            return await message.reply_photo(
+                random.choice(YUMI_PICS),
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
             )
@@ -58,7 +53,7 @@ async def start_pm(client, message: Message, _):
             if await is_on_off(2):
                 return await app.send_message(
                     chat_id=config.LOGGER_ID,
-                    text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>sᴜᴅᴏʟɪsᴛ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+                    text=f"✦ {message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>sᴜᴅᴏʟɪsᴛ</b>.\n\n<b>✦ ᴜsᴇʀ ɪᴅ ➠</b> <code>{message.from_user.id}</code>\n<b>✦ ᴜsᴇʀɴᴀᴍᴇ ➠</b> @{message.from_user.username}",
                 )
             return
         if name[0:3] == "inf":
@@ -96,33 +91,20 @@ async def start_pm(client, message: Message, _):
             if await is_on_off(2):
                 return await app.send_message(
                     chat_id=config.LOGGER_ID,
-                    text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+                    text=f"✦ {message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n✦ <b>ᴜsᴇʀ ɪᴅ ➠</b> <code>{message.from_user.id}</code>\n✦ <b>ᴜsᴇʀɴᴀᴍᴇ ➠</b> @{message.from_user.username}",
                 )
     else:
         out = private_panel(_)
-        AARVI = await message.reply_text(f"**कुछ अलग ही था**")
-        await AARVI.edit_text(f"**हमारे रिश्ते**")
-        await AARVI.edit_text(f"**का रंग**")
-        await AARVI.edit_text(f"**तुम्हारे जान**")
-        await AARVI.edit_text(f"**से वो अधूरा**")
-        await AARVI.edit_text(f"**रह गया**")
-        await AARVI.edit_text(f"**पर मेरे दिल**")
-        await AARVI.edit_text(f"**में आज भी वो**")
-        await AARVI.edit_text(f"**रंग कायम**")
-        await AARVI.edit_text(f"**है जोया जी**")
-        await AARVI.edit_text(f"**कुछ अलग ही था हमारे रिश्ते का रंग तुम्हारे जाने से वो अधूरा रह गया पर मेरे दिल में आज भी वो रंग कायम है जोया जी**")
-        await AARVI.edit_text(f"**❖ ʙᴀʙʏ ᴍᴜsɪᴄ sᴛᴀʀᴛᴇᴅ..**")
-        await AARVI.delete()
-        
-        await message.reply_video(
-            random.choice(NEXI_VID),
+        await message.reply_photo(
+            random.choice(YUMI_PICS),
             caption=_["start_2"].format(message.from_user.mention, app.mention),
+            # caption=_["start_2"].format(message.from_user.mention, app.mention, UP, DISK, CPU, RAM,served_users,served_chats),
             reply_markup=InlineKeyboardMarkup(out),
         )
         if await is_on_off(2):
             return await app.send_message(
                 chat_id=config.LOGGER_ID,
-                text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+                text=f"✦ {message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n✦ <b>ᴜsᴇʀ ɪᴅ ➠</b> <code>{message.from_user.id}</code>\n✦ <b>ᴜsᴇʀɴᴀᴍᴇ ➠</b> @{message.from_user.username}",
             )
 
 
@@ -131,8 +113,8 @@ async def start_pm(client, message: Message, _):
 async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
-    await message.reply_video(
-        random.choice(NEXI_VID),
+    await message.reply_photo(
+        random.choice(YUMI_PICS),
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
     )
@@ -166,9 +148,8 @@ async def welcome(client, message: Message):
                     return await app.leave_chat(message.chat.id)
 
                 out = start_panel(_)
-                await message.reply_video(
-                    random.choice(NEXI_VID),
-                    caption=_["start_3"].format(
+                await message.reply_text(
+                    text=_["start_3"].format(
                         message.from_user.mention,
                         app.mention,
                         message.chat.title,
